319298308c533728863abe36b9993afcc5b0d9bc
theRealSandro <assandroa95@gmail.com>
2018-11-21 18:51:23 +0700

    deployment commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 var  express = require('express');
 var  bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} =  require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
 // return value is function
 app.use(bodyParser.json());
 
@@ -33,9 +35,31 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-	res.send(req.params);
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		console.log('ID not valid');
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			console.log('Todo not found');
+			return res.status(404).send();
+		}
+			res.send(todo);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+	// validate id using isValid
+
+	// e status 404
+
+	// findById
+		// Success
+
+	// res.send(req.params);
 });
 
-app.listen(3000, () => {
-	console.log("Server running on port 3000")
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
 });
