e30dfce15856177d737a090d339f45fcc3e37f05
theRealSandro <assandroa95@gmail.com>
2018-11-21 19:44:17 +0700

    mongodb URI again

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGODB_URI);
 
 module.exports = {mongoose};
